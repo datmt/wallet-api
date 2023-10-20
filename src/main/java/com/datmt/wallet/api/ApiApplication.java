219f38ef12c186ed79ea605253ce4e5cd3ccc27a
Mạnh Đạt <dat.tm24@gmail.com>
2023-10-20 16:27:58 +0700

    optimize imports

--- src/main/java/com/datmt/wallet/api/ApiApplication.java
+++ src/main/java/com/datmt/wallet/api/ApiApplication.java
@@ -2,7 +2,6 @@ package com.datmt.wallet.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 
